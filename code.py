9432c3f17489738f6fa140a4b3882a8ae39e8739
Asaawari <61871556+Asaawari@users.noreply.github.com>
2021-01-05 14:09:00 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,29 +1,26 @@
-import csv
 import plotly.figure_factory as ff
 import statistics
 import random
 import pandas as pd
+import csv
 
-data = pd.read_csv("medium_data.csv")
-final_data = data["reading_time"].tolist()
-
-fig = ff.create_distplot([data],["reading_time"], show_hist=False)
+df = pd.read_csv("medium_data.csv")
+data = df["reading_time"].tolist()
+fig = ff.create_distplot([data], ["reading_time"], show_hist=False)
 fig.show()
-
-print("Population Mean = ",statistics.mean(final_data))
-
+print("Mean of population ",statistics.mean(data))
 def random_set_of_mean(counter):
     dataset = []
     for i in range(0, counter):
-        random_index= random.randint(0,len(final_data))
-        value = final_data[random_index]
+        random_index= random.randint(0,len(data))
+        value = data[random_index]
         dataset.append(value)
     mean = statistics.mean(dataset)
     return mean
 
 def show_fig(mean_list):
-    final_data = mean_list
-    fig = ff.create_distplot([final_data],["reading_time"], show_hist=False)
+    df = mean_list
+    fig = ff.create_distplot([df], ["reading_time"], show_hist=False)
     fig.show()
 
 def setup():
@@ -32,6 +29,5 @@ def setup():
         set_of_means= random_set_of_mean(10)
         mean_list.append(set_of_means)
     show_fig(mean_list)
-    print("Sampling Mean:- ", statistics.mean(mean_list))
-
+    print("Mean of sampling ", statistics.mean(mean_list))
 setup()
